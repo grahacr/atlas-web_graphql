15c881b8cdf7e6e4cb1702581873039dad4dca27
grahacr <courtney.graham@atlasschool.com>
2024-10-30 15:55:26 -0500

    set up graph ql schema and add to app

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt} = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -9,3 +9,24 @@ const TaskType = new GraphQLObjectType({
         description: { type: GraphQLString }
     }
 })
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        task: {
+            type: TaskType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parent, args) {
+
+            }
+        },
+    }
+});
+
+const schema = new GraphQLSchema({
+    query: RootQuery,
+});
+
+module.exports = schema;
