556a8e7ad47388108f05875911a6cda1f6955656
grahacr <courtney.graham@atlasschool.com>
2024-10-30 15:46:41 -0500

    setting up package installations and server directory plus schema for graphql

---
+++ server/schema/schema.js
@@ -0,0 +1,11 @@
+const { GraphQLObjectType, GraphQLString, GraphQLInt} = require('graphql');
+
+const TaskType = new GraphQLObjectType({
+    name: 'Task',
+    fields: {
+        id: {type: GraphQLString },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString }
+    }
+})
